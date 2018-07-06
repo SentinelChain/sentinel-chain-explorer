fdd70d45c05547d1c72bab238fae943a170f3e95
ndhngoc91 <ndhngoc91@gmail.com>
2018-07-06 10:09:59 +0700

    SC-59: Fix SC-74: Remove Ethereum references and adapt UX to Sentinel Chain

--- routes/signature.js
+++ routes/signature.js
@@ -8,20 +8,20 @@ router.get('/verify', function(req, res, next) {
 });
 
 router.post('/verify', function(req, res, next) {
-  var ethereumAddress = req.body.ethereumAddress.toLowerCase().replace("0x", "");
+  var sentinelAddress = req.body.sentinelAddress.toLowerCase().replace("0x", "");
   var message = req.body.message;
   var signature = req.body.signature;
   
-  if (!ethereumAddress) {
-    res.render('verifySignature', { result: { error: "Invalid Ethereum Address"}, message: message, signature: signature, ethereumAddress: ethereumAddress });
+  if (!sentinelAddress) {
+    res.render('verifySignature', { result: { error: "Invalid Sentinel Address"}, message: message, signature: signature, sentinelAddress: sentinelAddress });
     return;
   }
   if (!message) {
-    res.render('verifySignature', { result: { error: "Invalid Message"}, message: message, signature: signature, ethereumAddress: ethereumAddress });
+    res.render('verifySignature', { result: { error: "Invalid Message"}, message: message, signature: signature, sentinelAddress: sentinelAddress });
     return;
   }
   if (!signature) {
-    res.render('verifySignature', { result: { error: "Invalid Signature"}, message: message, signature: signature, ethereumAddress: ethereumAddress });
+    res.render('verifySignature', { result: { error: "Invalid Signature"}, message: message, signature: signature, sentinelAddress: sentinelAddress });
     return;
   }
   
@@ -31,15 +31,15 @@ router.post('/verify', function(req, res, next) {
     var recoveredPub = utils.ecrecover(msgSha, sigDecoded.v, sigDecoded.r, sigDecoded.s);
     var recoveredAddress = utils.pubToAddress(recoveredPub).toString("hex");
 
-    if (ethereumAddress === recoveredAddress) {
-      res.render('verifySignature', { result: { ok: "Signature is valid!"}, message: message, signature: signature, ethereumAddress: ethereumAddress });
+    if (sentinelAddress === recoveredAddress) {
+      res.render('verifySignature', { result: { ok: "Signature is valid!"}, message: message, signature: signature, sentinelAddress: sentinelAddress });
       return;
     } else {
-      res.render('verifySignature', { result: { error: "Signature is not valid!"}, message: message, signature: signature, ethereumAddress: ethereumAddress });
+      res.render('verifySignature', { result: { error: "Signature is not valid!"}, message: message, signature: signature, sentinelAddress: sentinelAddress });
       return;
     }
   } catch (e) {
-    res.render('verifySignature', { result: { error: "Error during signature verification: " + e}, message: message, signature: signature, ethereumAddress: ethereumAddress });
+    res.render('verifySignature', { result: { error: "Error during signature verification: " + e}, message: message, signature: signature, sentinelAddress: sentinelAddress });
     return;
   }
 });
